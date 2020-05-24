11f86507b4116b742c9ded3b1da4b16ca262189f
Paul Seyringer <p.seyringer@outlook.at>
2020-05-24 15:26:00 +0200

    Do host den Schaß, des Moi oba richtig

---
+++ TheGamblingMan_JohnnyFlex_android/pauli_seyringer/src/pauli_seyringer/HelloGarstigerWicht.java
@@ -0,0 +1,9 @@
+package pauli_seyringer;
+
+public class HelloGarstigerWicht {
+
+	public static void main(String[] args) {
+		System.out.println("Lassen Sie mich in Ruhe, Sie garstiger Wicht!");
+	}
+
+}
