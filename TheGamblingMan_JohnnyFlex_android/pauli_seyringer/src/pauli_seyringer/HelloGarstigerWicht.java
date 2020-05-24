f4df9aa7b4587da8b4afbb7321cfcbf5bb27ed0a
Daniel P <daniel@dpauli.at>
2020-05-24 15:32:02 +0200

    Some Code

--- TheGamblingMan_JohnnyFlex_android/pauli_seyringer/src/pauli_seyringer/HelloGarstigerWicht.java
+++ TheGamblingMan_JohnnyFlex_android/pauli_seyringer/src/pauli_seyringer/HelloGarstigerWicht.java
@@ -4,6 +4,16 @@ public class HelloGarstigerWicht {
 
 	public static void main(String[] args) {
 		System.out.println("Lassen Sie mich in Ruhe, Sie garstiger Wicht!");
+		
+		int age = 17;
+		int ageP = 20;
+		
+		if(age < 18 && ageP > 18){
+			System.out.println("It's not leagal to fuck girls younger than 18 brah");
+			
+		}
+		
+		
 	}
 
 }
